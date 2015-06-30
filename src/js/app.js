725ecd73ca4ae7e36452db3df1257e19051ab766
Alexander Pekkari <pekkarih@gmail.com>
2015-06-30 12:18:07 +0200

    better styling, re-enable transition on load

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+	// light weight mobile detection that should cover a majority of cases
+	var isMobile = function() {
+		return navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|BB10|Windows Phone|Tizen|Bada)/);
+	}
+
 	// Animate the page
 	setTimeout(function() {
-		$('section.full').removeClass('not-loaded');
+		$('.container').removeClass('not-loaded');
 	}, 100);
+	setTimeout(function() {
+		$('.second-wave').removeClass('not-loaded');
+	}, 400);
 });
